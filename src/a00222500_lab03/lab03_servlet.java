5d8d7d5bc0d95309a79c7129087a9c85a8ee5a08
Scott Robarts <scott.robarts@blinkdesigns.ca>
2011-05-06 20:31:50 -0700

    database object changes

--- src/a00222500_lab03/lab03_servlet.java
+++ src/a00222500_lab03/lab03_servlet.java
@@ -1,6 +1,8 @@
 package a00222500_lab03;
 
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.util.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,5 +30,17 @@ public class lab03_servlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	} 
+	
+	protected void getDatabaseParameters() {
+			String url = getInitParameter("url"); 
+			String driver = getInitParameter("driver"); 
+			String dbname = getInitParameter("dbname"); 
+			String user = getInitParameter("user"); 
+			String password = getInitParameter("password"); 
+			
+			Properties props = new Properties();
+			
+		}
+	}
 
 }
